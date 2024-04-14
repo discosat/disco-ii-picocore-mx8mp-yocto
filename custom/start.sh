c5bc250de9ac5f19cda35e20968494b9e4e54e1a
NValsted <nicolaj.valsted@live.dk>
2024-04-14 21:56:32 +0200

    add u-boot and n-boot to deployable files

--- custom/start.sh
+++ custom/start.sh
@@ -35,5 +35,7 @@ sudo cp -L /build/yocto-fus/build-fsimx8mp-fus-imx-wayland/tmp/deploy/images/fsi
 sudo cp -L /build/yocto-fus/build-fsimx8mp-fus-imx-wayland/tmp/deploy/images/fsimx8mp/emmc-fsimx8mp.sysimg /deploy/
 sudo cp -L /build/yocto-fus/build-fsimx8mp-fus-imx-wayland/tmp/deploy/images/fsimx8mp/Image-fsimx8mp /deploy/
 sudo cp -L /build/yocto-fus/build-fsimx8mp-fus-imx-wayland/tmp/deploy/images/fsimx8mp/update.scr /deploy/
+sudo cp -L /build/yocto-fus/build-fsimx8mp-fus-imx-wayland/tmp/deploy/images/fsimx8mp/uboot-fsimx8mp.fs /deploy/
+sudo cp /build/yocto-fus/sources/meta-fus/recipes-bsp/u-boot/files/nboot-disco-mod.fs.gz /deploy/ && sudo gunzip /deploy/nboot-disco-mod.fs.gz && sudo mv /deploy/nboot-disco-mod.fs /deploy/nboot-fsimx8mp.fs
 
 echo "System files ready to flash onto a PicoCore™MX8MP are now available in the deploy directory"
